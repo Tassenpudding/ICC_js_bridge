8f30d93e10e5cf24d76bb68741592a33b600fcf2
Tassenpudding <mgfreak@web.de>
2019-11-19 15:27:21 +0100

    rename ICClib.h to libicc.h

--- cppsrc/main.cpp
+++ cppsrc/main.cpp
@@ -2,7 +2,7 @@
 #include <napi.h>
 #include <stdlib.h>
 #include <dlfcn.h>
-#include "ICClib.h"
+#include "libicc.h"
 #include <string>
 #include <iostream>
 
@@ -13,7 +13,7 @@ libicc* libHandle;
 Napi::String doPow(const Napi::CallbackInfo& info)	{
 	Napi::Env env = info.Env();
 	if (info.Length() != 2 || !info[0].IsString() || !info[1].IsNumber() ) {
-		Napi::TypeError::New(env, "String expected").ThrowAsJavaScriptException();
+		Napi::TypeError::New(env, "Wrong Input Type").ThrowAsJavaScriptException();
 	}
 	Napi::String n_api_trytes = info[0].As<Napi::String>();	
 	string trytes = n_api_trytes.Utf8Value();	
@@ -32,7 +32,7 @@ Napi::String doPow(const Napi::CallbackInfo& info)	{
 Napi::String generateAddress(const Napi::CallbackInfo& info){ //string seed, int index, int security) {
 	Napi::Env env = info.Env();
 	if (info.Length() != 3 || !info[0].IsString() || !info[1].IsNumber() || !info[2].IsNumber()) {
-		Napi::TypeError::New(env, "String or Number expected").ThrowAsJavaScriptException();
+		Napi::TypeError::New(env, "Wrong Input Type").ThrowAsJavaScriptException();
 	}
 	Napi::String n_api_seed = info[0].As<Napi::String>();
 	string seed = n_api_seed.Utf8Value();	
@@ -48,29 +48,12 @@ Napi::String generateAddress(const Napi::CallbackInfo& info){ //string seed, int
 	return returnValue;
 }
 
-/*Napi::Object deInit(){
-	destroy(libHandle);
-}*/
-
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-	//void *handle;
-
-	//handle = dlopen("/home/pi/lib/ICC_lib.so", RTLD_LAZY);
-	//if (handle == NULL) {
-	//	printf("dlopen failed\r\n");
-	//}
-
-	//ICC_lib* (*create)();
-	//void (*destroy)(ICC_lib*);
-
-	//create = (ICC_lib* (*)())dlsym(handle, "create_object");
-	//destroy	= (void (*)(ICC_lib*))dlsym(handle, "destroy_object");
 
 	libHandle	= (libicc*) create_libicc();
 	
 	exports.Set("doPow", Napi::Function::New(env, doPow));
 	exports.Set("generateAddress", Napi::Function::New(env, generateAddress));
-	//exports.Set("deInit", Napi::Function::New(env, deInit));	
 	
 	return exports;
 }
